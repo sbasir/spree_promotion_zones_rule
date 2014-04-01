5ceab4b7dcafc98f39e9aacfe2af8011c9139ded
Sofy Basir <sofy.basir@gmail.com>
2014-04-01 22:55:48 +1100

    oops fixed the assets path js

--- app/assets/javascripts/spree/backend/spree_promotion_zones_rule.js
+++ app/assets/javascripts/spree/backend/spree_promotion_zones_rule.js
@@ -1,4 +1,4 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
-//= require zone_picker
+//= require_tree .
